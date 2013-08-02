ac2d708f310873e97c9edc694508a2d2c385a970
Atilla Ozgur <ati.ozgur@gmail.com>
2013-08-02 19:15:28 +0300

    powershell script guncelleme

--- build_nuget_Karkas.Templates.Mvc4.ps1
+++ build_nuget_Karkas.Templates.Mvc4.ps1
@@ -5,6 +5,6 @@ $nugetCacheFolder = "$Home/AppData/Local/NuGet/Cache"
 $nugetPackageName = "$projectName.$version.nupkg"
 #mkdir tools
 #mkdir content
-../packages/NuGet.CommandLine.2.5.0/tools/NuGet pack  $projectName/$projectName.nuspec -Version $version
+./bin/NuGet.CommandLine.2.5.0/tools/NuGet pack  $projectName.nuspec -Version $version
 move $nugetPackageName  $nugetCacheFolder -Force
 write-host "moved $nugetPackageName to $nugetCacheFolder "
